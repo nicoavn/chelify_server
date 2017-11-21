e25389cc0dee37a28e4d603d97e0d9f310118a77
Alvin D <nicoavn@gmail.com>
2017-11-21 13:40:47 -0400

    WIP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,10 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $data = [
+            'key' => session()->all()
+        ];
+        return response()->json($data);
         return view('home');
     }
 }
