dbcf2bf5d2c213b6a9547ac36f6aa30b57f49d06
Alvin <nicoavn@gmail.com>
2018-01-12 15:05:48 +0000

    WIP Groups, Alter Transactions - add description, Contributions, Transactions

--- app/Account.php
+++ app/Account.php
@@ -25,7 +25,7 @@ class Account extends Model
 
     public function transactions()
     {
-        return $this->hasManyThrough('App\Transaction', 'App\FinancialInstrument');
+        return $this->hasManyThrough('App\Transaction', 'App\FinancialInstrument')->orderByDesc('created_at');
     }
 
     public function type()
