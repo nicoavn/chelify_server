a753084f2f73e7bb3f52504e3388d6968c2d6bbe
Alvin D <aduran@encorerepair.com>
2018-01-15 19:10:51 -0400

    WIP

--- database/migrations/2018_01_15_190543_alter_group_user_add_compound_primary_key.php
+++ database/migrations/2018_01_15_190543_alter_group_user_add_compound_primary_key.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterGroupUserTableAddUniqueGroupUser extends Migration
+class AlterGroupUserAddCompoundPrimaryKey extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AlterGroupUserTableAddUniqueGroupUser extends Migration
     public function up()
     {
         Schema::table('group_user', function (Blueprint $table) {
-            $table->unique(['user_id', 'group_id'], 'group_user_unique');
+            $table->primary(['user_id', 'group_id']);
+            //$table->unique(['user_id', 'group_id'], 'group_user_unique');
         });
     }
 
@@ -25,8 +26,6 @@ class AlterGroupUserTableAddUniqueGroupUser extends Migration
      */
     public function down()
     {
-//        Schema::table('group_user', function (Blueprint $table) {
-//            $table->dropUnique('group_user_unique');
-//        });
+        //
     }
 }
