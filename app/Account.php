8ebf0523efe6daa0109a2b4eac8b1a7126534b62
Alvin D <aduran@encorerepair.com>
2018-01-18 17:37:37 -0400

    Fix Recurrent Transaction

--- app/Account.php
+++ app/Account.php
@@ -40,7 +40,7 @@ class Account extends Model
 
     public function recurrentTransactions()
     {
-        return $this->hasManyThrough('App\RecurrentTransaction', 'App\FinancialInstrument', 'a', 'b');
+        return $this->hasManyThrough('App\RecurrentTransaction', 'App\FinancialInstrument', 'account_id', 'charge_to');
     }
 
     public function financialInstruments()
